03b17668688e6bd2bedf987f2530836423c5abae
Zac Garby <zacgarby@icloud.com>
2017-11-08 21:50:55 +0000

    Remove fmt.Display for Operator

--- src/parser/ast.rs
+++ src/parser/ast.rs
@@ -1,5 +1,3 @@
-use std::fmt;
-
 // The various types of operators.
 #[allow(unused)]
 pub enum Operator {
@@ -7,17 +5,6 @@ pub enum Operator {
     Subtract,
     Multiply,
     Divide,
-}
-
-impl fmt::Display for Operator {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", match self {
-            &Operator::Add      => "+",
-            &Operator::Subtract => "-",
-            &Operator::Multiply => "*",
-            &Operator::Divide   => "/",
-        })
-    }
 }
 
 #[allow(dead_code)]
